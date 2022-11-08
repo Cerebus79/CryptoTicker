cfb8f30e41dd468b91a5af6985af51e354b64384
Cerebus79 <daniel@vestry.com>
2022-11-08 21:24:04 +0000

    commit

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -14,10 +16,15 @@ namespace API.Controllers;
 public class AccountController : ControllerBase
 {
     private DataContext _context;
+    private UserManager<AppUser> _usermanager;
+    private SignInManager<AppUser> _signInManager;
 
-    public AccountController(DataContext context)
+    public AccountController(DataContext context, 
+    UserManager<AppUser> usermanager, SignInManager<AppUser> signInManager)
     {
         _context = context;
+        _usermanager = usermanager;
+        _signInManager = signInManager;
     }
 
     [HttpGet("watchlist/{id}")]
@@ -29,5 +36,27 @@ public class AccountController : ControllerBase
 
         //return await _context.WatchList.ToListAsync<WatchList>();
 
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
+    {
+        var user = await _usermanager.FindByEmailAsync(loginDto.Email);
+
+        if(user == null) return Unauthorized();
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if(result.Succeeded)
+        {
+            return new UserDto{
+                DisplayName = user.DisplayName,
+                Token = "this will be a token",
+                Username = user.UserName,
+                Image = null
+            };
+        }
+
+        return Unauthorized();
     }
 }
--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -10,5 +10,5 @@ public class UserDto
     public string DisplayName{get;set;}
     public string Token{get; set;}
     public string Username{get;set;}
-    public string Image {get; set;}
+    public string? Image {get; set;}
 }
